ddaa13215e451588b5463a11291bec4a641cf2c0
rkone1552000 <65998347+rkone1552000@users.noreply.github.com>
2022-03-31 00:22:57 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+// -------------- For first image ---------- //
+var randomNumber1 = Math.floor((Math.random()*6)+1);
+    //console.log(randomNumber1);
+var randomDiceImage1 = "images/dice" + randomNumber1 + ".png";  //dice1.png to dice6.png
+
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomDiceImage1);
+
+// -------------- For second image ---------- //
+var randomNumber2 = Math.floor(Math.random()*6) + 1;
+var randomDiceImage2 = "images/dice" + randomNumber2 + ".png";
+
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src", randomDiceImage2);
+
+if(randomNumber1 > randomNumber2)
+{
+    document.querySelector("h1").innerHTML = "🏆Player 1 Wins";
+}
+else if(randomNumber1 < randomNumber2)
+{
+    document.querySelector("h1").innerHTML = "Player 2 Wins🏆";
+}
+else
+{
+    document.querySelector("h1").innerHTML = "It's a Draw!!";
+}
